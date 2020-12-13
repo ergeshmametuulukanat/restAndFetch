a7c7ad2421f699d51edd740aa9f855e6492564b8
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2020-12-13 19:50:43 +0600

    fixed addRole in service

--- src/main/java/com/company/service/UserServiceImpl.java
+++ src/main/java/com/company/service/UserServiceImpl.java
@@ -1,17 +1,23 @@
 package com.company.service;
 
+import com.company.model.Role;
 import com.company.model.User;
+import com.company.repository.RoleRepository;
 import com.company.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
 //    @Autowired
 //    private PasswordEncoder passwordEncoder;
@@ -24,6 +30,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public User save(User user) {
 //        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Set<Role> roles = new HashSet<>();
+        Set<Role> userRoles = user.getRoles();
+        for (int i = 0; i < user.getRoles().size(); i++) {
+            for (Role role : userRoles) {
+                roles.add(roleRepository.findById(role.getId()).get());
+            }
+        }
+        user.setRoles(roles);
         return userRepository.save(user);
     }
 
